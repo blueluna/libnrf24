c4ea17d8ffb06fda095db47b7b851fd516244106
Erik Svensson <erik.public@gmail.com>
2013-09-24 21:26:03 +0200

    Fix bad comments.

--- src/sleep.c
+++ src/sleep.c
@@ -13,7 +13,7 @@ void nrf24_msleep(const uint32_t milliseconds)
 {
 	struct timespec req;
 	req.tv_sec = 0;
-	req.tv_nsec = milliseconds * 1000000L; // convert to nanoseconds
+	req.tv_nsec = milliseconds * 1000000L; /* convert to nanoseconds */
 	nanosleep(&req, (struct timespec *)NULL);
 }
 
@@ -21,6 +21,6 @@ void nrf24_usleep(const uint32_t microseconds)
 {
 	struct timespec req;
 	req.tv_sec = 0;
-	req.tv_nsec = microseconds * 1000L; // convert to nanoseconds
-	nanosleep(&req, (struct timespec *)NULL);  
+	req.tv_nsec = microseconds * 1000L; /* convert to nanoseconds */
+	nanosleep(&req, (struct timespec *)NULL);
 }
